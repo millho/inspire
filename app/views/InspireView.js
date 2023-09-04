f3be9b911271697a24cb5d23cdb9c93e84760b2c
Jason Millhorn <jdmillhorn@gmail.com>
2023-09-03 20:22:20 -0600

    stuck

--- app/views/InspireView.js
+++ app/views/InspireView.js
@@ -7,20 +7,23 @@ export const InspireView = /*HTML*/`
 <div class="container-fluid">
     <section class="row">
         <div class="col-4">
-            <section class="row">
-                <form class="" onsubmit="app.TodoController.createTodo()">
-                    <div class="mb-3">
-                        <input required name="description" type="description" placeholder="Enter Todo" class="form-control" id="inputTodo">
-                        <button type="submit" class="btn btn-info text-light elevation-3"><i class="mdi mdi-folder-plus-outline"></i></button>
-                    </div>
-                </form>
-
-                <div id="todo-list">
+            <form class="" onsubmit="app.TodoController.createTodo()">
+                <div class="mb-3">
+                    <input required name="description" type="description" placeholder="Enter Todo" class="form-control" id="inputTodo">
+                    <button type="submit" class="btn btn-info text-light elevation-3"><i class="mdi mdi-folder-plus-outline"></i></button>
                 </div>
-            </section>
+            </form>
+
+            <div id="todo-list">
+            </div>
         </div>
+
         <div class="col-4" id="quote">
         <div>
+
+        <div class="col-4" id="weather">
+        
+        </div>
     </section>
 </div>
 `
